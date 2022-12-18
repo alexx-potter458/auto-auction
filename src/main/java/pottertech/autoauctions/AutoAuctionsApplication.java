9e2e69afed6c2cae42d34d833e0bb48e76397cc8
alexx-potter458 <alexx.potter458@gmail.com>
2022-12-18 14:34:46 +0200

    first commit

---
+++ src/main/java/pottertech/autoauctions/AutoAuctionsApplication.java
@@ -0,0 +1,13 @@
+package pottertech.autoauctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoAuctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoAuctionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pottertech/autoauctions/AutoAuctionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pottertech.autoauctions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoAuctionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
